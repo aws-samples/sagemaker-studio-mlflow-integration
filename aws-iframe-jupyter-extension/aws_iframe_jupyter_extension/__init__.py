05bdd792be1f98bba9ef11e56e3b2f169259073a
Paolo Di Francesco <frpaolo@amazon.at>
2023-03-05 14:13:30 +0000

    Remove aws-iframe extension

--- aws-iframe-jupyter-extension/aws_iframe_jupyter_extension/__init__.py
+++
@@ -1,6 +0,0 @@
-def _jupyter_server_extension_paths():
-    return [{"module": "aws_iframe_jupyter_extension"}]
-
-
-def _jupyter_nbextension_paths():
-    return []
--- aws-iframe-jupyter-extension/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { initiateExtension } from "./aws_iframe_extension";
-
-export const getPaths = async (region: string): Promise<string[]> => {
-  return [""];
-};
-
-const extension = initiateExtension(getPaths);
-
-export default extension;
